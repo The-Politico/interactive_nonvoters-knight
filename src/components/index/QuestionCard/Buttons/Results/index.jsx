6a1787fad861d9da84e0a3cdd6bddacc74a72a3f
Beatrice Jin <beatricezjin@gmail.com>
2020-02-11 15:14:11 -0500

    bars

--- src/components/index/QuestionCard/Buttons/Results/index.jsx
+++ src/components/index/QuestionCard/Buttons/Results/index.jsx
@@ -12,7 +12,52 @@ class Results extends React.Component {
     return (
       <div className={styles.component + ' class-name'}>
         { userChoice !== null ?
-          <p className='summary'>You answered more like <span className={`voter-${voter}`}>{voter ? 'voters' : 'nonvoters'}</span>.</p>
+          <div>
+            <p className='summary'>You answered more like <span className={`voter-${voter}`}>{voter ? 'voters' : 'nonvoters'}</span>.</p>
+
+            <h4> Voters </h4>
+            <div className='bar voter'>
+              {
+                data.map((a, i) =>
+                  <div className={`segment choice-${i}`}
+                    style={{ width: `${a.Voter}%`}}
+                    key={`key-${i}`}
+                  />
+                )
+              }
+            </div>
+            <div className='stats voter'>
+              {
+                data.map((a, i) =>
+                  <p className={`stat stat-${i} bold-${userChoice === a && voter}`}>
+                    {`${a.Voter}% ${a.Answer}`}
+                  </p>
+                )
+              }
+            </div>
+
+            <h4> Nonvoters </h4>
+            <div className='bar nonvoter'>
+              {
+                data.map((a, i) =>
+                  <div className={`segment choice-${i}`}
+                    style={{ width: `${a.Nonvoter}%`}}
+                    key={`key-${i}`}
+                  />
+                )
+              }
+            </div>
+
+            <div className='stats nonvoter'>
+              {
+                data.map((a, i) =>
+                  <p className={`stat stat-${i} bold-${userChoice === a && !voter}`}>
+                    {`${a.Nonvoter}% ${a.Answer}`}
+                  </p>
+                )
+              }
+            </div>
+          </div>
         : null}
       </div>
     );
